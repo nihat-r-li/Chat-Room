a30c510d8315edcceb94126714edded4432779e8
nihat123445 <148061050+nihat123445@users.noreply.github.com>
2023-10-21 13:09:58 +0400

    Update server.py

--- server.py
+++ server.py
@@ -5,20 +5,23 @@ from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import padding
 
-# Server configuration
 HOST = '127.0.0.1'
 PORT = 12345
-clients = []
+clients = []  # List to store connected client sockets
+
+# Generation of RSA key pair for encryption
 private_key = rsa.generate_private_key(
     public_exponent=65537,
     key_size=2048,
 )
 public_key = private_key.public_key()
 
+# This is function to broadcast encrypted messages to all connected clients
 def broadcast_encrypted_message(client_socket, message):
     for client in clients:
         if client != client_socket:
             try:
+                # Here we encrypt and send message
                 ciphertext = client.encrypt(
                     message.encode('utf-8'),
                     padding.OAEP(
@@ -29,12 +32,15 @@ def broadcast_encrypted_message(client_socket, message):
                 )
                 client.send(ciphertext)
             except:
+                # Remove client in case of an error
                 client.close()
                 remove_client(client)
 
+# This is function to handle a client's messages
 def handle_client(client_socket):
     while True:
         try:
+            # Here we receive and decrypt message
             ciphertext = client_socket.recv(2048)
             if ciphertext:
                 plaintext = private_key.decrypt(
@@ -51,6 +57,7 @@ def handle_client(client_socket):
         except:
             continue
 
+# This is function to remove a client from list
 def remove_client(client_socket):
     if client_socket in clients:
         clients.remove(client_socket)
@@ -62,6 +69,7 @@ server.listen(5)
 
 print("Chat server is running on port " + str(PORT))
 
+# Here we create threads to handle them
 while True:
     client_socket, client_addr = server.accept()
     clients.append(client_socket)
